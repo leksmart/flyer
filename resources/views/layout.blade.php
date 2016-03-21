441b8c3488f5dccf55bfb6bf6cdc634309784069
Arek Bochenski <bochenski@gmail.com>
2016-03-21 07:25:42 +0700

    setting up the project

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Project Flyer</title>
+        <link href="/css/app.css" rel="stylesheet"/>
+    </head>
+
+    <body>
+        <div class="container">
+            @yield('content')
+
+        </div>
+    </body>
+
+</html>
