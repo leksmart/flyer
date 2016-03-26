ea27ee285b9f058ce271da511d8e25a0f24cdfd7
Arek Bochenski <bochenski@gmail.com>
2016-03-26 18:01:53 +0700

    next 8

---
+++ app/Flyer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flyer extends Model
+{
+
+    
+
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
+}
